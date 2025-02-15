b852cd50aa6de459546f081981a40513b913c1eb
atique-shahriar <satique06@gmail.com>
2025-02-15 17:00:12 +0600

    Update banner section - Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from '@/components/sections/Navbar'
 import Banner from '@/components/sections/Banner'
+import Country from '@/components/sections/Country'
 
 export default function Home() {
     return (
         <div>
             <Navbar />
             <Banner />
+            <div className="w-3/4 mx-auto">
+                <Country />
+            </div>
         </div>
     )
 }
